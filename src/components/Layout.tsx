2a6806ab8d1e70c1e048853e9edc877a4ae2cd66
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2022-02-26 14:48:39 +0530

    Layout update for landing page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,12 +6,15 @@ interface Props {}
 export const Layout: React.FC<Props> = ({ children }) => {
   const isPlayerPage = useMatch("/quizes/:id");
   const isQuestionsPage = useMatch("/quizes/:id/questions");
+  const isLandingPage = useMatch("/");
   return (
     <div className="flex flex-1 flex-col min-h-screen">
       <NavBar />
       <div
         className={`flex-1 xl:w-full ${
-          isPlayerPage || isQuestionsPage ? "" : "lg:max-w-screen-xl mx-4 "
+          isPlayerPage || isQuestionsPage || isLandingPage
+            ? ""
+            : "lg:max-w-screen-xl mx-4 "
         }xl:mx-auto`}
       >
         {children}
