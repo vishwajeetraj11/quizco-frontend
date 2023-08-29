69380d3d598d482d0643a75eced894b391236065
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2023-08-30 00:52:29 +0530

    Update urls.ts

--- src/shared/urls.ts
+++ src/shared/urls.ts
@@ -1,5 +1,5 @@
 // export const baseURL = 'https://quizco.herokuapp.com/api/v1';
-export const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api/v1' : 'https://quizco.herokuapp.com/api/v1';
+export const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api/v1' : 'https://quizco-backend.onrender.com/api/v1';
 
 
 export const endpoints = {
